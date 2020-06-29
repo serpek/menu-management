3471d29009ce87b51af01ba72b3173dcc69e452c
root <root@admin.serpek.com>
2020-06-29 09:07:36 +0300

    add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name: "server",
+    script: "./dist/apps/server/main.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }/*,{
+    name       : "client",
+    script     : "./api.js",
+    instances  : 4,
+    exec_mode  : "cluster"
+  }*/]
+}
