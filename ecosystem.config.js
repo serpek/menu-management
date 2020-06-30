56bb9354fbf85fceee81740f896968db4c8d5649
Mustafa SERPEK <serpek@gmail.com>
2020-06-30 19:40:47 +0300

    First commit - Deployment configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "server",
     script: "npm",
-    args: ["-- run nest:start"],
+    args: "-- run nest:start",
     watch: true,
     env: {
       NODE_ENV: "development",
